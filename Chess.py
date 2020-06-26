3f7a645d4b35affa4cf42553c474dffce5c96cc0
DrYoshiyahu <josiahhilbig@gmail.com>
2020-06-26 23:21:50 +1000

    Update Chess.py

--- Chess.py
+++ Chess.py
@@ -1,4 +1,6 @@
 from collections import deque
+from tkinter import *
+import math
 import itertools
 import heapq
 import time
@@ -12,9 +14,16 @@ import time
 
 
 # CONFIGURATION
-INITIAL_BOARD = [1, 2, -1, 2, 3, 0, -1, 3, -1, 0, 1, 0, 3, 0, 1, 0, -1, 3, -1, 0, 3, 2, -1, 2, 1]
-WINNING_BOARD = [0, 1, -1, 2, 0, 0, -1, 2, -1, 0, 1, 0, 0, 0, 1, 0, -1, 2, -1, 0, 0, 1, -1, 2, 0]
-
+INITIAL_BOARD = [2,3,1,2,-1,
+                 3,2,2,-1,-1,
+                 1,1,3,3,-1,
+                 -1,3,-1,1,0,
+                 0,3,3,3,2]
+WINNING_BOARD = [0,2,1,1,-1,
+                 2,2,1,-1,-1,
+                 1,0,0,0,-1,
+                 -1,0,-1,2,0,
+                 0,0,0,2,0]
 
 # SPEEDUP OR SPEEDUP_EXP, NOT both!
 SPEEDUP = False  # May be turned on for speed, Heuristic: Out of Position
@@ -24,11 +33,20 @@ SPEEDUP_EXP = True  # Non-admissible heuristic, very fast, probably NOT optimal
 assert len(WINNING_BOARD) == len(INITIAL_BOARD)
 assert len(WINNING_BOARD) in [9, 12, 16, 20, 25]
 
-print("Starting..")
+print("Starting...")
 
+master = Tk();
+canvas_width = 1200;
+canvas_height = 960;
+drawing = Canvas(master, width=canvas_width, height=canvas_height);
+drawing.pack();
 
-LEN_X = 1
-LEN_Y = 1
+x_position = 0;
+y_position = 0;
+max_x = (canvas_width/120)-1;
+
+LEN_X = 1;
+LEN_Y = 1;
 
 if len(WINNING_BOARD) == 9:
     LEN_X = 3
@@ -164,25 +182,54 @@ def bfs(node):
         k += 1
 
 
-def board_print(board):
+def board_print(board,parent):
     print("\n")
+    cell_size = 100/LEN_X;
+    origin_x = (x_position * 120)+10;
+    origin_y = (y_position * 120)+10;
+    global line_x;
+    global line_y;
+    line_x = [0,0];
+    line_y = [0,0];
+    drawing.create_rectangle(origin_x,origin_y,origin_x+100,origin_y+100);
+    for i in range(len(board)):
+        position_x = (i%LEN_X)*cell_size;
+        position_y = math.floor(i/LEN_Y)*cell_size;
+        drawing.create_rectangle(origin_x+position_x,origin_y+position_y,origin_x+position_x+cell_size,origin_y+position_y+cell_size);
+        if (board[i] == -1):
+            drawing.create_rectangle(origin_x+position_x,origin_y+position_y,origin_x+position_x+cell_size,origin_y+position_y+cell_size,fill="#333333");
+        elif (board[i] == 1):
+            drawing.create_oval(origin_x+position_x,origin_y+position_y,origin_x+position_x+cell_size,origin_y+position_y+cell_size,fill="#ff5555")
+        elif (board[i] == 2):
+            drawing.create_oval(origin_x+position_x,origin_y+position_y,origin_x+position_x+cell_size,origin_y+position_y+cell_size,fill="#5555ff")
+        elif (board[i] == 3):
+            drawing.create_oval(origin_x+position_x,origin_y+position_y,origin_x+position_x+cell_size,origin_y+position_y+cell_size,fill="#aaaa55")
+        if (parent) and (board[i] != parent.board[i]):
+            if (line_x == [0,0]):
+                line_x = [origin_x+position_x+(cell_size/2),origin_y+position_y+(cell_size/2)];
+            else:
+                line_y = [origin_x+position_x+(cell_size/2),origin_y+position_y+(cell_size/2)];
+    drawing.create_line(line_x[0],line_x[1],line_y[0],line_y[1],width=2);
     for i in range(LEN_Y):
-        print(board[i * LEN_X:(i + 1) * LEN_X])
+        print(board[i * LEN_X:(i + 1) * LEN_X]);
 
+start_time = time.time();
 
-start_time = time.time()
+root = ChessBoard(INITIAL_BOARD, None, 0);
+winner = bfs(root);
+a = 0;
 
-root = ChessBoard(INITIAL_BOARD, None, 0)
-winner = bfs(root)
-
-a = 0
 while winner is not None:
-    a += 1
-    board_print(winner.board)
-
-    winner = winner.parent
-
+    a += 1;
+    board_print(winner.board,winner.parent);
+    winner = winner.parent;
+    if (x_position < max_x):
+        x_position += 1;
+    else:
+        x_position = 0;
+        y_position += 1;
 
 print("--- %s seconds ---" % (time.time() - start_time))
 print("Win in {} Moves. If SPEEDUP_EXP == True, this may NOT be optimal.".format(a - 1))
 
+mainloop();
